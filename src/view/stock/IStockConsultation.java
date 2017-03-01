85304f19e14939bbebba2aebbbcb5abc58f82ac8
Ovelny <this.is.ovelny@gmail.com>
2017-03-01 14:09:12 +0100

    view boilerplate

--- src/view/stock/IStockConsultation.java
+++ src/view/stock/IStockConsultation.java
@@ -1,5 +1,16 @@
 package view.stock;
 
 public interface IStockConsultation {
-
+	
+	public List<Product> listProducts() {
+		// TODO
+	}
+	
+	public Product getProduct() {
+		// TODO
+	}
+	
+	public List<Product> searchProduct() {
+		// TODO
+	}
 }
--- src/view/stock/IStockManagement.java
+++ src/view/stock/IStockManagement.java
@@ -1,5 +1,36 @@
 package view.stock;
 
 public interface IStockManagement {
-
+	
+	public Category addCategory() {
+		// TODO
+	}
+	
+	public Category updateCategory() {
+		// TODO
+	}
+	
+	public void deleteCategory() {
+		// TODO
+	}
+	
+	public ArrayList<Category> getCategories() {
+		// TODO
+	}
+	
+	public Product addProduct() {
+		// TODO
+	}
+	
+	public void deleteProduct() {
+		// TODO
+	}
+	
+	public ArrayList<Product> listProducts() {
+		// TODO
+	}
+	
+	public Product getProduct() {
+		// TODO
+	}
 }
