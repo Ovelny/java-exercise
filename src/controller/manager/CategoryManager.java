921077c887fdc081381ea4afe77be7d7dfadc551
Ovelny <this.is.ovelny@gmail.com>
2017-04-16 00:01:40 +0200

    getCategory by ID and Label

--- src/controller/manager/CategoryManager.java
+++ src/controller/manager/CategoryManager.java
@@ -3,12 +3,14 @@ package controller.manager;
 import model.Category;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CategoryManager {
 	
 	private static int idCategory = 0;
 	private String label;
     private static ArrayList<Category> categories = new ArrayList<>();
+
 
 	public static Category addCategory(String label) {
 	    idCategory = idCategory + 1;
@@ -17,11 +19,32 @@ public class CategoryManager {
 	    return newCategory;
     }
 
+
     public static ArrayList<Category> getCategories() {
 	    return categories;
     }
 
-    public static ArrayList<Category> getCategoriesById(int id) {
-	    return null;
+
+    public static Category getCategoryById(int id) {
+        List<Category> objects = CategoryManager.getCategories();
+        for (int i = 0; i < objects.size(); i++) {
+            if (objects.get(i).getId() == id) {
+                return objects.get(i);
+            }
+        }
+        // TODO : add try / catch
+        return null;
+    }
+
+
+    public static Category getCategoryByLabel(String label) {
+        List<Category> objects = CategoryManager.getCategories();
+        for (int i = 0; i < objects.size(); i++) {
+            if (objects.get(i).getLabel().equals(label)) {
+                return objects.get(i);
+            }
+        }
+        // TODO : add try / catch
+        return null;
     }
 }
--- src/view/Program.java
+++ src/view/Program.java
@@ -1,16 +1,20 @@
 package view;
 import controller.StockController;
 import controller.manager.CategoryManager;
+import model.Category;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Program {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello, World");
         StockController.loadProductsFromFile();
-		ArrayList<model.Category> test = CategoryManager.getCategories();
-		System.out.println(test);
+        Category test = CategoryManager.getCategoryByLabel("vinBien");
+        //Category test = CategoryManager.getCategoryById(1);
+        System.out.println(test);
 	}
-	
+
 }
